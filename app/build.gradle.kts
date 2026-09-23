ca56a31465ae6fdf2908ac8f70a81a4088f3f1fa
LeVa3bat <alex.666.881@gmail.com>
2026-09-23 07:25:33 +0300

    Bump Sklad PRO to 0.3.0 alpha3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     minSdk = 24
     targetSdk = 34
     // Standalone universal product. It never updates/replaces "Каптёрка ПРО".
-    versionCode = 2
-    versionName = "0.2.0-alpha2"
+    versionCode = 3
+    versionName = "0.3.0-alpha3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
